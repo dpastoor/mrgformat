c675a6879fab463cfc3f0039c447911fc316029e
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-21 19:01:46 -0500

    work for both annotated blocks with values and annotated blocks with no values

--- R/normalize_annotated_block.R
+++ R/normalize_annotated_block.R
@@ -15,6 +15,7 @@ annotation_spacing <- function(block_text, block_position, max_positions, i = 1)
 #' @param text the text to replace
 #' @export
 normalize_annotated_block <- function(text) {
+
     block_positions <- stringr::str_locate_all(text, ":")
 
     max_positions <- c(value_start = 0, annotation_start = 0)
@@ -30,22 +31,23 @@ normalize_annotated_block <- function(text) {
     })
 
     text <- purrr::map2(text, block_positions, annotation_spacing, max_positions, 1)
+    # if full block with values will also have an annotation start
+    if (max_positions[["annotation_start"]]) {
+        block_positions <- stringr::str_locate_all(text, ":")
 
-    block_positions <- stringr::str_locate_all(text, ":")
+        max_positions <- c(value_start = 0, annotation_start = 0)
 
-    max_positions <- c(value_start = 0, annotation_start = 0)
-
-    lapply(block_positions, function(block) {
-        if (length(block)) {
-            for (row in seq_along(1:nrow(block))) {
-                if (max_positions[row] < block[row, 1]) {
-                    max_positions[row] <<- block[row, 1]
+        lapply(block_positions, function(block) {
+            if (length(block)) {
+                for (row in seq_along(1:nrow(block))) {
+                    if (max_positions[row] < block[row, 1]) {
+                        max_positions[row] <<- block[row, 1]
+                    }
                 }
             }
-        }
-    })
-
+        })
     text <- purrr::map2(text, block_positions, annotation_spacing, max_positions, 2)
+    }
     return(unlist(text))
 
 }
@@ -53,7 +55,11 @@ normalize_annotated_block <- function(text) {
 #' addin
 #' @export
 normalize_annotated_block_addin <- function() {
-    text <- rstudioapi::getActiveDocumentContext()$content
-    new_text <- normalize_annotated_block(text)
+    context <- rstudioapi::getActiveDocumentContext()
+    text <- context$content
+    range_start <- context$selection[[1L]]$range$start[[1L]]
+    range_end   <- context$selection[[1L]]$range$end[[1L]]
+
+    new_text <- normalize_annotated_block(text[range_start:range_end])
     rstudioapi::insertText(paste0(new_text, collapse = "\n"))
 }
